858178654cbe8cd7e774d4d3aea478c0d896969b
Jordi Martí Muñoz <jordimarti@me.com>
2016-01-28 10:04:34 +0100

    Canvis header PDF

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -16,7 +16,7 @@ class DocumentsController < ApplicationController
     PDFKit.configure do |config|
       config.default_options = {
         :page_size     => 'A4',
-        :margin_top    => '0.7in',
+        :margin_top    => '1in',
         :margin_right  => '0.7in',
         :margin_bottom => '0.8in',
         :margin_left   => '0.7in'
