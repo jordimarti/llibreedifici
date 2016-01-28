a706d511bb6d2449bd56b2f907610f5da0ef42c6
Jordi Martí Muñoz <jordimarti@me.com>
2016-01-28 10:25:30 +0100

    Canvis en header i footer del PDF per edificis nous i existents

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -20,14 +20,18 @@ class DocumentsController < ApplicationController
 
   def manual_habitatge_pdf
     url_edifici = 'http://llibreedifici.herokuapp.com/edificis/' + @edifici.id.to_s + '/documents/vista_pdf_manual_habitatge?locale=ca'
-    kit = PDFKit.new(url_edifici)
+    url_header = 'http://llibreedifici.herokuapp.com/edificis/' + @edifici.id.to_s + '/documents/vista_pdf_header?locale=ca'
+    url_footer = 'http://llibreedifici.herokuapp.com/edificis/' + @edifici.id.to_s + '/documents/vista_pdf_footer?locale=ca'
+    kit = PDFKit.new(url_edifici, :header_html => url_header, :footer_html => url_footer, :header_spacing => 5, :footer_spacing => 5)
     file = kit.to_file(Rails.root + 'tmp/' + 'demo.pdf')
     send_file file, filename: "#{@edifici.nom_edifici}_manual_habitatge.pdf", disposition: 'attachment'
   end
 
   def llibre_existent_pdf
     url_edifici = 'http://llibreedifici.herokuapp.com/edificis/' + @edifici.id.to_s + '/documents/vista_pdf_edifici_existent?locale=ca'
-    kit = PDFKit.new(url_edifici)
+    url_header = 'http://llibreedifici.herokuapp.com/edificis/' + @edifici.id.to_s + '/documents/vista_pdf_header?locale=ca'
+    url_footer = 'http://llibreedifici.herokuapp.com/edificis/' + @edifici.id.to_s + '/documents/vista_pdf_footer?locale=ca'
+    kit = PDFKit.new(url_edifici, :header_html => url_header, :footer_html => url_footer, :header_spacing => 5, :footer_spacing => 5)
     file = kit.to_file(Rails.root + 'tmp/' + 'demo.pdf')
     send_file file, filename: "#{@edifici.nom_edifici}.pdf", disposition: 'attachment'
   end
