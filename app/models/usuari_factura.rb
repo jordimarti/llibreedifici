052edd9ea64288f4d3bb22006191615c3e8dd021
Jordi Martí Muñoz <jordimarti@me.com>
2017-07-03 14:36:29 +0200

    Fa que la validació de NIF només passi si la lletra és majúscula

--- app/models/usuari_factura.rb
+++ app/models/usuari_factura.rb
@@ -3,7 +3,7 @@ class UsuariFactura < ActiveRecord::Base
 	belongs_to :edifici
 
 	validates :nif, presence: true
-	validates :nif, format: { with: /\A[0-9]{8}[A-Za-z]\Z/,
+	validates :nif, format: { with: /\A[0-9]{8}[A-Z]\Z/,
     message: "NIF incorrecte" }
 
 end
