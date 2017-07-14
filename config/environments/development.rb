3777e5624c3b2c5a8e44a622c8894b8ebe13350d
Jordi Martí Muñoz <jordimarti@me.com>
2017-07-14 09:48:32 +0200

    Canvia smtp a sendgrid

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,13 +46,13 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'gmail.com',
-    user_name:            'jordimarti007@gmail.com',
-    password:             '36531630v',
-    authentication:       'plain',
-    enable_starttls_auto: true  }
+   :address              => "smtp.sendgrid.net",
+   :port                 => 587,
+   :user_name            => ENV['sendgrid_username'],
+   :password             => ENV['sendgrid_password'],
+   :authentication       => "plain",
+   :enable_starttls_auto => true
+  }
 
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,11 +82,19 @@ Rails.application.configure do
   #}
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
-  config.action_mailer.delivery_method = :smtp
+  
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
-  config.action_mailer.default_options = {from: 'no-reply@apabcn.cat'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+   :address              => "smtp.sendgrid.net",
+   :port                 => 587,
+   :user_name            => ENV['sendgrid_username'],
+   :password             => ENV['sendgrid_password'],
+   :authentication       => "plain",
+   :enable_starttls_auto => true
+  }
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
