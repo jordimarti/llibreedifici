ace62f8cf9d650d2ecbef68a8e8d2c85af31824d
Jordi Martí Muñoz <jordimarti@me.com>
2018-06-06 15:58:24 +0200

    Afegeix més validacions a les operacions de manteniment personalitzades

--- app/models/operacio.rb
+++ app/models/operacio.rb
@@ -1,5 +1,9 @@
 class Operacio < ActiveRecord::Base
 
 	validates :periodicitat, :presence => true, numericality: true
+	validates :periodicitat_text_ca, :presence => true
+	validates :periodicitat_text_es, :presence => true
+	validates :descripcio_ca, :presence => true
+	validates :descripcio_es, :presence => true
 
 end
