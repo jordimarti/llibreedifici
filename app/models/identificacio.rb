2b08fa22aefeb7ccd9e99194858f6863451b6543
Jordi Martí Muñoz <jordimarti@me.com>
2019-01-09 08:25:40 +0100

    Canvi d'estils en la generació de documents

--- app/models/identificacio.rb
+++ app/models/identificacio.rb
@@ -11,7 +11,7 @@ class Identificacio < ActiveRecord::Base
   has_attached_file :foto_facana_3, :styles => { :original => "600x600>", :medium => "300x300>", :thumb => "100x100!" }, :default_url => "https://s3-eu-west-1.amazonaws.com/llibreedifici/missing/missing_facana.png"
   validates_attachment_content_type :foto_facana, :content_type => /\Aimage\/.*\Z/
 
-  has_attached_file :logo_empresa, :styles => { :original => "600x600>", :medium => "300x300>", :thumb => "200x40>" }, :default_url => "https://s3-eu-west-1.amazonaws.com/llibreedifici/missing/missing_logo.png"
+  has_attached_file :logo_empresa, :styles => { :original => "600x600>", :medium => "300x300>", :thumb => "125x25>" }, :default_url => "https://s3-eu-west-1.amazonaws.com/llibreedifici/missing/missing_logo.png"
   validates_attachment_content_type :logo_empresa, :content_type => /\Aimage\/.*\Z/
 
 	belongs_to :edifici
